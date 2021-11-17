7f7757043544cd8a16a2210240d8301345a4733a
vincentole <63076449+vincentole@users.noreply.github.com>
2021-11-17 10:55:17 +0100

    updated nav animation

--- sunnyside_agency_landing_page/js/main.js
+++ sunnyside_agency_landing_page/js/main.js
@@ -5,11 +5,13 @@ const mainNavTriangle = document.querySelector(".main-nav__triangle")
 
 const mediaQuerySmall = window.matchMedia('(min-width: 730px)');
 
-mobileBtn.addEventListener("click", handleMobile);
+mobileBtn.addEventListener("click", handleMobileNavBtn);
 
-function handleMobile() {
+function handleMobileNavBtn() {
     mainNav.classList.toggle("nav-mobile");
     mainNavTriangle.classList.toggle("nav-mobile");
+    setTimeout(() => mainNavBar.classList.toggle("opacity-1"),100);
+    setTimeout(() => mainNavTriangle.classList.toggle("opacity-1"),100);
 }
 
 mediaQuerySmall.addEventListener("change", handleSmallMQ)
@@ -18,6 +20,8 @@ function handleSmallMQ() {
     if (mediaQuerySmall.matches) {
         mainNav.classList.remove("nav-mobile");
         mainNavTriangle.classList.remove("nav-mobile");
+        mainNavBar.classList.remove("opacity-1");
+        mainNavTriangle.classList.remove("opacity-1");
     }   
 }
 
