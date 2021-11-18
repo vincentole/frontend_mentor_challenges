57832d969ba37ecb52f356a6a53c887ce02166a4
vincentole <63076449+vincentole@users.noreply.github.com>
2021-11-18 20:52:52 +0100

    added stats preview card component

---
+++ stats_preview_card_component/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	purge: ["./**/*.html"],
+	darkMode: false, // or 'media' or 'class'
+	mode: "jit",
+	theme: {
+		extend: {
+			screens: {
+				sm: "600px",
+			},
+			colors: {
+				primary: {
+					darkBlue: "hsl(233, 47%, 7%)",
+					desatBlue: "hsl(244, 38%, 16%)",
+					softViolet: "hsl(277, 64%, 61%)",
+				},
+				neutral: {
+					white: "hsl(0, 0%, 100%)",
+					whiteA75: "hsla(0, 0%, 100%, 0.75)",
+					whiteA60: "hsla(0, 0%, 100%, 0.6)",
+				},
+			},
+      fontFamily: {
+        inter: "'Inter', sans-serif",
+        lexend: "'Lexend Deca', sans-serif",
+      },
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
