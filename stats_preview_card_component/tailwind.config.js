52d1c9e975c220c4fa18a98d852f5a5375d8ff97
vincentole <63076449+vincentole@users.noreply.github.com>
2021-11-18 22:05:16 +0100

    added img color overlay

--- stats_preview_card_component/tailwind.config.js
+++ stats_preview_card_component/tailwind.config.js
@@ -12,6 +12,7 @@ module.exports = {
 					darkBlue: "hsl(233, 47%, 7%)",
 					desatBlue: "hsl(244, 38%, 16%)",
 					softViolet: "hsl(277, 64%, 61%)",
+					softVioletA60: "hsla(277, 74%, 35%, 0.6)",
 				},
 				neutral: {
 					white: "hsl(0, 0%, 100%)",
